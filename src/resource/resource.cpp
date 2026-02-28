a02f059e1f79d80883a4cbda26c9c41ca3eb0d89
sub4ikgg <zerotanker33@gmail.com>
2026-03-01 01:21:44 +0300

    Implemented resource conf handling

--- src/resource/resource.cpp
+++ src/resource/resource.cpp
@@ -2,20 +2,33 @@
 
 #include <Arduino.h>
 #include <HTTPClient.h>
+#include <Preferences.h>
 #include "../debug.h"
 #include "../led/led.h"
 #include "../ble/ble.h"
 
-const int HTTP_OK = 200;
 const int BOOT_BUTTON_PIN = 0;
+
+static const char* PREFS_NAMESPACE       = "res_prefs";
+static const char* PREFS_KEY_URL         = "res_url";
+static const char* PREFS_KEY_CODE        = "res_code";
+static const char* DEFAULT_RESOURCE_URL  = "https://httpbin.org/status/200";
+const int DEFAULT_EXPECTED_CODE          = 200;
+
+static String _url;
+static int _expectedCode = -1;
+
+static void loadPrefsIfNeeded();
 
 void checkResourceAvailability() {
   if (digitalRead(BOOT_BUTTON_PIN) == LOW && isBleAdvertising) return;
 
+  loadPrefsIfNeeded();
+
   LOG("[Resource] Checking resource availability...");
 
   HTTPClient http;
-  http.begin("https://httpbin.org/status/200");
+  http.begin(_url);
 
   int code = http.GET();
   http.end();
@@ -25,7 +38,7 @@ void checkResourceAvailability() {
     return;
   }
 
-  if (code == HTTP_OK) {
+  if (code == _expectedCode) {
     toggleRedPin(false);
 
     toggleGreenPin(true);
@@ -39,4 +52,25 @@ void checkResourceAvailability() {
     LOG("[Resource] Not available (" + String(code) + ")");
     resourceIsNotAvailableBlink();
   }
+}
+
+static void loadPrefsIfNeeded() {
+  if (_expectedCode != -1) return;
+
+  Preferences prefs;
+  prefs.begin(PREFS_NAMESPACE, true);
+  _url          = prefs.getString(PREFS_KEY_URL, DEFAULT_RESOURCE_URL);
+  _expectedCode = prefs.getInt(PREFS_KEY_CODE, DEFAULT_EXPECTED_CODE);
+  prefs.end();
+}
+
+void writeResourceConf(String url, int expectedCode) {
+  Preferences prefs;
+  prefs.begin(PREFS_NAMESPACE);
+  prefs.putString(PREFS_KEY_URL, url);
+  prefs.putInt(PREFS_KEY_CODE, expectedCode);
+  prefs.end();
+
+  _url = url;
+  _expectedCode = expectedCode;
 }
--- src/resource/resource.h
+++ src/resource/resource.h
@@ -1,6 +1,9 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
+#include <Arduino.h>
+
 void checkResourceAvailability();
+void writeResourceConf(String url, int expectedCode);
 
 #endif
